b2d8a5c54bf5a74cd8c300ae65d3b1c1501952fd
SonvanJune <sonthu3333@gmail.com>
2023-09-25 10:59:50 +0700

    clean

--- src/main/java/com/chatapp/entity/BaseEntity.java
+++ src/main/java/com/chatapp/entity/BaseEntity.java
@@ -2,9 +2,7 @@ package com.chatapp.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
@@ -24,12 +22,4 @@ public abstract class BaseEntity {
 	@Column(name = "modifieddate", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
 	@LastModifiedDate
 	private Date updatedAt = new Date();
-	
-	// @Column(name = "createdby")
-	// @CreatedBy
-	// private String createdBy;
-	
-	// @Column(name = "modifiedby")
-	// @LastModifiedBy
-	// private String modifiedBy;
 }
