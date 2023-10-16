72ec0b2d00a25a5acab3fe8919238c58eeecc924
SonvanJune <sonthu3333@gmail.com>
2023-10-16 09:51:24 +0700

    post comment socket

--- src/main/java/com/chatapp/controller/socket/CommentSocketController.java
+++ src/main/java/com/chatapp/controller/socket/CommentSocketController.java
@@ -18,14 +18,14 @@ public class CommentSocketController {
     @Autowired
     private PostService postService;
 
-    @MessageMapping({"/posts/{postId}/comments", "/messages/{postId}/comments/"})
+    @MessageMapping({"/posts/{postId}/comments", "/posts/{postId}/comments/"})
     @SendTo({"/topic/posts/{postId}", "/topic/posts/{postId}"})
     public List<CommentResponeseDTO> saveMessage(@RequestBody CommentSaveRequestDTO commentSaveRequestDTO) {
         postService.commentPost(commentSaveRequestDTO);
         return postService.findCommentByPostId(commentSaveRequestDTO.getPostId());
     }
 
-    @MessageMapping({"/posts/{postId}/comments/listen", "/messages/{postId}/comments/listen/"})
+    @MessageMapping({"/posts/{postId}/comments/listen", "/posts/{postId}/comments/listen/"})
     @SendTo({"/topic/posts/{postId}", "/topic/posts/{postId}"})
     public List<CommentResponeseDTO> getMessages(@DestinationVariable("postId") Long postId){;
         return postService.findCommentByPostId(postId);
