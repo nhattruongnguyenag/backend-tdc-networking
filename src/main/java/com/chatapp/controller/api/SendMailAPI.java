2eccf77b3255233f7846b17899c086850b132cc8
Nguyen Nhat Truong <nhattruongnguyenag@gmail.com>
2023-11-18 13:12:40 +0700

    add send mail

---
+++ src/main/java/com/chatapp/controller/api/SendMailAPI.java
@@ -0,0 +1,30 @@
+package com.chatapp.controller.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chatapp.dto.request.EmailSendRequestionDTO;
+import com.chatapp.service.EmailService;
+
+@RestController
+@RequestMapping("/api")
+public class SendMailAPI {
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping({ "/mail", "/mail/" })
+    public ResponseEntity sendMail(@RequestBody EmailSendRequestionDTO emailSendRequestionDTO) {
+        try {
+            emailService.sendEmail(emailSendRequestionDTO.getTo(), emailSendRequestionDTO.getSubject(),
+                    emailSendRequestionDTO.getText());
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
---
+++ src/main/java/com/chatapp/dto/request/EmailSendRequestionDTO.java
@@ -0,0 +1,10 @@
+package com.chatapp.dto.request;
+
+import lombok.Data;
+
+@Data
+public class EmailSendRequestionDTO {
+    private String to;
+    private String subject;
+    private String text;
+}
