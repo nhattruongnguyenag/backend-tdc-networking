657844cb71402e47d8ac5dd3cae068efa5e1792b
SonvanJune <sonthu3333@gmail.com>
2023-12-10 11:31:30 +0700

    update socket

--- src/main/java/com/chatapp/controller/socket/PostSocketController.java
+++ src/main/java/com/chatapp/controller/socket/PostSocketController.java
@@ -22,15 +22,17 @@ public class PostSocketController {
 
     @MessageMapping({ "/posts/group/{code}/listen/{userlogin}", "/posts/group/{code}/listen/{userlogin}/" })
     @SendTo({ "/topic/posts/group/{code}", "/topic/posts/group/{code}/" })
-    public List<PostSearchResponseDTO> getPostsByCode(@DestinationVariable("code") String code , @DestinationVariable("userLogin") Long userLogin) {
-        return postService.findAllByGroupCode(code,userLogin);
+    public List<PostSearchResponseDTO> getPostsByCode(@DestinationVariable("code") String code,
+            @DestinationVariable("userLogin") Long userLogin) {
+        return postService.findAllByGroupCode(code, userLogin);
     }
 
-    @MessageMapping({ "/posts/group/{code}/like", "/posts/group/{code}/like/"})
+    @MessageMapping({ "/posts/group/{code}/like", "/posts/group/{code}/like/" })
     @SendTo({ "/topic/posts/group/{code}", "/topic/posts/group/{code}" })
-    public List<PostSearchResponseDTO> saveMessage(@DestinationVariable("code") String code ,@RequestBody LikeRequestDTO likeRequestDTO) {
+    public List<PostSearchResponseDTO> saveMessage(@DestinationVariable("code") String code,
+            @RequestBody LikeRequestDTO likeRequestDTO) {
         postService.likePost(likeRequestDTO);
-        return postService.findAllByGroupCode(code,likeRequestDTO.getUserId());
+        return postService.findAllByGroupCode(code, likeRequestDTO.getUserId());
     }
 
     @MessageMapping({ "/posts/save/user/unsave", "/posts/save/user/unsave/" })
@@ -49,8 +51,11 @@ public class PostSocketController {
 
     @MessageMapping({ "/posts/save/user/search/{search}/unsave", "/posts/save/user/search/{search}/unsave/" })
     @SendTo({ "/topic/posts/save/search", "/topic/posts/save/search" })
-    public List<PostSearchResponseDTO> userSavePostSearch(@RequestBody UserSavePostRequestDTO userSavePostRequestDTO , @DestinationVariable("search") String search) {
-        postService.userSavePost(userSavePostRequestDTO);
+    public List<PostSearchResponseDTO> userSavePostSearch(@RequestBody UserSavePostRequestDTO userSavePostRequestDTO,
+            @DestinationVariable("search") String search) {
+        if (userSavePostRequestDTO.getPostId() != null) {
+            postService.userSavePost(userSavePostRequestDTO);
+        }
         UserSavePostFindRequestDTO userSavePostFindRequestDTO = new UserSavePostFindRequestDTO();
         userSavePostFindRequestDTO.setSearch(search);
         userSavePostFindRequestDTO.setUserId(userSavePostRequestDTO.getUserId());
@@ -59,8 +64,11 @@ public class PostSocketController {
 
     @MessageMapping({ "/posts/save/user/search/{search}/like", "/posts/save/user/search/{search}/like/" })
     @SendTo({ "/topic/posts/save/search", "/topic/posts/save/search" })
-    public List<PostSearchResponseDTO> userLikePostSearch(@RequestBody LikeRequestDTO likeRequestDTO , @DestinationVariable("search") String search) {
-        postService.likePost(likeRequestDTO);
+    public List<PostSearchResponseDTO> userLikePostSearch(@RequestBody LikeRequestDTO likeRequestDTO,
+            @DestinationVariable("search") String search) {
+        if (likeRequestDTO.getPostId() != null) {
+            postService.likePost(likeRequestDTO);
+        }
         UserSavePostFindRequestDTO userSavePostFindRequestDTO = new UserSavePostFindRequestDTO();
         userSavePostFindRequestDTO.setSearch(search);
         userSavePostFindRequestDTO.setUserId(likeRequestDTO.getUserId());
@@ -69,8 +77,9 @@ public class PostSocketController {
 
     @MessageMapping({ "/posts/group/{code}/unsave", "/posts/group/{code}/unsave/" })
     @SendTo({ "/topic/posts/group/{code}", "/topic/posts/group/{code}/" })
-    public List<PostSearchResponseDTO> userSavePostInGroupScreen(@RequestBody UserSavePostRequestDTO userSavePostRequestDTO , @DestinationVariable("code") String code) {
+    public List<PostSearchResponseDTO> userSavePostInGroupScreen(
+            @RequestBody UserSavePostRequestDTO userSavePostRequestDTO, @DestinationVariable("code") String code) {
         postService.userSavePost(userSavePostRequestDTO);
-        return postService.findAllByGroupCode(code,userSavePostRequestDTO.getUserId());
+        return postService.findAllByGroupCode(code, userSavePostRequestDTO.getUserId());
     }
 }
