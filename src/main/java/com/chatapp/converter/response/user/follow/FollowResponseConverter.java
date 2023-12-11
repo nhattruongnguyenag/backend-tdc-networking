4ebf31453ec2db0286790ce17676664a9b20a40f
SonvanJune <sonthu3333@gmail.com>
2023-12-11 09:04:54 +0700

    test

--- src/main/java/com/chatapp/converter/response/user/follow/FollowResponseConverter.java
+++ src/main/java/com/chatapp/converter/response/user/follow/FollowResponseConverter.java
@@ -4,7 +4,6 @@ import com.chatapp.converter.abstracts.BaseConverter;
 import com.chatapp.dto.response.user.follow.UserFollowResponseDTO;
 import com.chatapp.entity.FollowEntity;
 import com.chatapp.entity.UserEntity;
-import com.chatapp.repository.FollowReposittory;
 import com.chatapp.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +14,21 @@ public class FollowResponseConverter extends BaseConverter<FollowEntity, UserFol
     @Autowired
     UserRepository userRepository;
 
-    @Autowired
-    FollowReposittory followReposittory;
-
     @Override
     public UserFollowResponseDTO toDTO(FollowEntity entity) {
         UserFollowResponseDTO userFollowResponseDTO = super.toDTO(entity);
-        UserEntity userEntity = userRepository.findOneById(entity.getUserFollow().getId());
+        UserEntity userEntity = userRepository.findOneById(entity.getUser().getId());
         userFollowResponseDTO.setId(userEntity.getId());
         userFollowResponseDTO.setImage(userEntity.getImage());
         userFollowResponseDTO.setName(userEntity.getName());
 
-        if (userEntity.getFollowUsers().contains(followReposittory
-                .findOneByUser_IdAndFollow_Id(entity.getUser().getId(), entity.getUserFollow().getId()))) {
-            userFollowResponseDTO.setIsFollow(true);
-        } else {
-            userFollowResponseDTO.setIsFollow(false);
-        }
+        // for (FollowEntity followEntity : userEntity.getFollowUsers()) {
+        //     if (followEntity.getUser().getId() == entity.getUser().getId()) {
+        //         userFollowResponseDTO.setIsFollow(true);
+        //         break;
+        //     }
+        //     userFollowResponseDTO.setIsFollow(false);
+        // }
         return userFollowResponseDTO;
     }
 }
