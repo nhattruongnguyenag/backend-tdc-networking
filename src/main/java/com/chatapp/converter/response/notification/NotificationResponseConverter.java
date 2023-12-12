30bd3802c632779afcfac24d101cbbca08db248b
SonvanJune <sonthu3333@gmail.com>
2023-12-12 18:00:44 +0700

    test

--- src/main/java/com/chatapp/converter/response/notification/NotificationResponseConverter.java
+++ src/main/java/com/chatapp/converter/response/notification/NotificationResponseConverter.java
@@ -7,6 +7,7 @@ import com.chatapp.dto.response.notification.NotificationResponseDTO;
 import com.chatapp.dto.response.post.PostSearchResponseDTO;
 import com.chatapp.entity.NotificationEntity;
 import com.chatapp.entity.PostEntity;
+import com.chatapp.enums.Notification;
 import com.chatapp.repository.PostRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,13 +29,18 @@ public class NotificationResponseConverter extends BaseConverter<NotificationEnt
     public NotificationResponseDTO toDTO(NotificationEntity entity) {
         NotificationResponseDTO notificationResponseDTO = super.toDTO(entity);
         notificationResponseDTO.setUser(userInfoResponseConverter.toDTO(entity.getUser()));
-        // if (entity.getData() != null && !entity.getData().equals("")) {
-        //     String id = entity.getData().split(":")[1];
-        //     PostEntity postEntity = postRepository.findOneById(Long.valueOf(id));
-        //     postEntity.setUserLogin(entity.getUser().getId());
-        //     PostSearchResponseDTO postSearchResponseDTO = postSearchResponseConverter.toDTO(postEntity);
-        //     notificationResponseDTO.setDataValue(postSearchResponseDTO);
-        // }
+        if (entity.getData() != null && entity.getData() != "") {
+            if (!entity.getType().equals(Notification.USER_APPLY_JOB.getValue())) {
+                String id = entity.getData().split(":")[1];
+                PostEntity postEntity = postRepository.findOneById(Long.valueOf(id));
+                postEntity.setUserLogin(entity.getUser().getId());
+                PostSearchResponseDTO postSearchResponseDTO = postSearchResponseConverter.toDTO(postEntity);
+                notificationResponseDTO.setDataValue(postSearchResponseDTO);
+            }
+            else{
+                notificationResponseDTO.setDataValue(null);
+            }
+        }
         return notificationResponseDTO;
     }
 }
