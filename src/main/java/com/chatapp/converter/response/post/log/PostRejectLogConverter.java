3a28cc8f6697d77aa4c72f7fc6c9d1fd79dc0ff4
SonvanJune <sonthu3333@gmail.com>
2023-12-13 10:07:52 +0700

    add group to post reject log

--- src/main/java/com/chatapp/converter/response/post/log/PostRejectLogConverter.java
+++ src/main/java/com/chatapp/converter/response/post/log/PostRejectLogConverter.java
@@ -1,17 +1,30 @@
 package com.chatapp.converter.response.post.log;
 
 import com.chatapp.converter.abstracts.BaseConverter;
+import com.chatapp.converter.response.group.GroupResponseConverter;
+import com.chatapp.dto.response.group.GroupResponseDTO;
 import com.chatapp.dto.response.post.log.PostRejectLogDTO;
 import com.chatapp.entity.PostApprovalLogEntity;
+import com.chatapp.repository.GroupRepository;
 import com.chatapp.util.DateTimeUtil;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PostRejectLogConverter extends BaseConverter<PostApprovalLogEntity, PostRejectLogDTO> {
+    @Autowired
+    GroupResponseConverter groupResponseConverter;
+
+    @Autowired
+    GroupRepository groupRepository;
+
     @Override
     public PostRejectLogDTO toDTO(PostApprovalLogEntity postApprovalLogEntity) {
         PostRejectLogDTO dto = super.toDTO(postApprovalLogEntity);
         dto.setCreatedAt(DateTimeUtil.convertToTimestamp(postApprovalLogEntity.getCreatedAt()));
+        GroupResponseDTO groupResponseDTO = groupResponseConverter.toDTO(groupRepository.findOneById(postApprovalLogEntity.getPost().getGroup().getId()));
+        dto.setGroup(groupResponseDTO);
         return dto;
     }
 }
