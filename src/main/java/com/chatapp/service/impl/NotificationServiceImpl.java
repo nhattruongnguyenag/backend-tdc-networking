ef4f7f577af2aabbae5a4a6e03a9a4c9e511fe5f
SonvanJune <sonthu3333@gmail.com>
2023-12-15 10:45:27 +0700

    test bug

--- src/main/java/com/chatapp/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/chatapp/service/impl/NotificationServiceImpl.java
@@ -47,8 +47,7 @@ public class NotificationServiceImpl implements NotificationService {
         if (userRepository.findOneById(notificationByUserRequestDTO.getId()) == null) {
             throw new DuplicateUsernameException("user_not_exists");
         }
-        return notificationResponseConverter
-                .toDTOGroup(notificationRepository.findByUser_IdOrderByUpdatedAtDesc(notificationByUserRequestDTO.getId()));
+        return null;
     }
 
     @Override
