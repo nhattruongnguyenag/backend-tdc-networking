b13fa61d47c9a81b1597b1641cb3dda6851a31f8
SonvanJune <sonthu3333@gmail.com>
2023-12-17 21:03:40 +0700

    update app

--- src/main/java/com/chatapp/dto/response/post/log/PostRejectLogResponseDTO.java
+++ src/main/java/com/chatapp/dto/response/post/log/PostRejectLogResponseDTO.java
@@ -7,4 +7,37 @@ public class PostRejectLogResponseDTO {
     private String content;
     private String createdAt;
     private PostSearchResponseDTO post;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public PostSearchResponseDTO getPost() {
+        return this.post;
+    }
+
+    public void setPost(PostSearchResponseDTO post) {
+        this.post = post;
+    }
+
 }
